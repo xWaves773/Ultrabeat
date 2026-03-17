68d15a2f89815ec01b0e4c5b20b8fc2525c90b4c
xWaves773 <dominik.drenth@gmail.com>
2026-03-17 13:54:08 +0100

    feat: camera and player movement

---
+++ Ultrabeat/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,44 @@
+//Tutorial that I used:https://youtu.be/swOfmyJvb98?si=FnuUb9Xatvsdfstt
+
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+
+    public float mouseSensitivity = 500f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+
+    void Start()
+    {
+       //Locks the cursor in the middle of the screen 
+       Cursor.lockState = CursorLockMode.Locked;
+
+
+    }
+
+    void Update()
+    {
+        //Getting mouse inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Rotate around the x axis (up & down)
+        xRotation -= mouseY;
+
+        //Clamp rotation
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        //Rotate around the x axis (left & right)
+        yRotation += mouseX;
+
+        //Apply rotations to transform
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+
+    }
+}
---
+++ Ultrabeat/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+//Tutorial that I used:https://youtu.be/swOfmyJvb98?si=FnuUb9Xatvsdfstt
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f * 2;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+    bool isMoving;
+
+    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
+
+    void Start()
+    {
+      controller = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        //Ground check   
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //Resetting teh default velocity
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //Getting inputs
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Creating the moving vector
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //Actually moving the player
+        controller.Move(move * speed * Time.deltaTime);
+
+        //Check if the player can jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //Falling down
+        velocity.y += gravity * Time.deltaTime;
+
+        //Exectuting the jump
+        controller.Move(velocity * Time.deltaTime);
+
+        if (lastPosition != gameObject.transform.position && isGrounded == true)
+        {
+            isMoving = true;
+            //for later use
+        }
+        else
+        {
+            isMoving = false;
+            //for later use
+        }
+
+        lastPosition = gameObject.transform.position;
+
+    }
+}
